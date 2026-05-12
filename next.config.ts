224fa8e4e8447f263caaa588e9d34dccba1a8cc3
Chinmay Panda <chinmay@openautonomyx.com>
2026-05-12 03:26:47 -0700

    fix: configure GitHub Pages base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from 'next';
 
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isGithubPages ? '/website' : '',
+  assetPrefix: isGithubPages ? '/website/' : '',
 };
 
 export default nextConfig;
